cd8535dddafa612330652f445327552115b09fbc
Marek Polewski <marek@logbook.li>
2025-06-18 08:18:48 +0200

    number of views on video is an int

--- src/brave/types/web/videos.py
+++ src/brave/types/web/videos.py
@@ -1,5 +1,4 @@
-from typing import List
-from typing import Optional
+from typing import List, Union, Optional
 
 from pydantic import BaseModel
 from pydantic import Field
@@ -13,7 +12,7 @@ class VideoData(BaseModel):
     """A model representing metadata gathered for a video."""
 
     duration: Optional[str] = Field(default=None, description="A time string representing the duration of the video.")
-    views: Optional[str] = Field(default=None, description="The number of views of the video.")
+    views: Optional[int] = Field(default=None, description="The number of views of the video.")
     creator: Optional[str] = Field(default=None, description="The creator of the video.")
     publisher: Optional[str] = Field(default=None, description="The publisher of the video.")
     thumbnail: Optional[Thumbnail] = Field(default=None, description="A thumbnail associated with the video.")
